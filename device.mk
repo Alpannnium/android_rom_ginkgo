489545a08bbbe17a45598801f55c9c5e7edae909
AlpannVerse <revanzakindar2008@gmail.com>
2025-09-05 21:16:04 +0700

    Create device.mk

---
+++ device.mk
@@ -0,0 +1,8 @@
+# Minimal device makefile
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_ginkgo.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_ginkgo-user \
+    lineage_ginkgo-userdebug \
+    lineage_ginkgo-eng
