4e358b4c2e2d369d3660c027e6a655d594d700b4
AlpannVerse <revanzakindar2008@gmail.com>
2025-09-05 21:13:34 +0700

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -1,6 +1,6 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := ginkgo_dummy
-LOCAL_SRC_FILES := dummy.c
+LOCAL_MODULE := ginkgo
+LOCAL_SRC_FILES := ginkgo.c
 include $(BUILD_SHARED_LIBRARY)
